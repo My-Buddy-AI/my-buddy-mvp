bb52809b3d074d19c94c58ef8a2b59c697aad382
Semyon <39240102+inkviz96@users.noreply.github.com>
2024-03-20 12:55:23 +0000

    Update whatsapp_task.py

--- api/tasks/whatsapp_task.py
+++ api/tasks/whatsapp_task.py
@@ -24,7 +24,7 @@ def send_whatsapp_response(data: dict, end_user, sender_id):
             streaming=False
         )
         token = "EAAFJMpu0YYYBO4Hd5Amc2dHSCSqQfBHuZCBiQlMZA15mxl2ZAQ97FbuYseuZAUUe4RUrCKgy29kPE84ZCZBRkqltmZCF2Dc0XTFkUIzZAVC61XvdQDSx5VyabZBfKSCSpAYsylmtUq8LZC67CczI0ZAGoVs7Pa2iRsQlVC7BD8UOXTAkp2zNzDM5NP6HMzi8xZBqWngMikqIl0481ODHfQbnqxf1W6eLla66cuiI8wwZD"
-        whatsapp_app = whatsapp_service(phone_number_id="159045903960749", bearer_token=token)
+        whatsapp_app = whatsapp_service(phone_number_id="159045903960749", bearer_token=token, version='v18.0')
         response = compact_response(response)
         logging.info(f"Compact response: {response}")
         response_data = response.json
